e8f20e221170e55d6ed65164657181216e6fe9f8
Narasimha P <pasupuletinarasimha999@gmail.com>
2024-05-28 19:20:40 +0530

    Update tools.tf

--- Terraform/eks-argocd-setup/tools.tf
+++ Terraform/eks-argocd-setup/tools.tf
@@ -9,20 +9,20 @@ resource "helm_release" "argocd" {
   values           = [file("values/argocd.yaml")]
   depends_on = [aws_eks_node_group.private-nodes]
 }
-module "nginx-controller" {
-  source  = "terraform-iaac/nginx-controller/helm"
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress-controller"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "nginx-ingress-controller"
 
-  additional_set = [
-    {
-      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
-      value = "nlb"
-      type  = "string"
-    },
-    {
-      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-cross-zone-load-balancing-enabled"
-      value = "true"
-      type  = "string"
-    }
-  ]
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
+
+  set {
+    name  = "controller.ingressClass"
+    value = "nginx"
+  }
+
   depends_on = [aws_eks_node_group.private-nodes]
 }
