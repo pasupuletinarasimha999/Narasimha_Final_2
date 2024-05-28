674e1a416bc3484851de0fcab37f27f2309daca1
Narasimha P <pasupuletinarasimha999@gmail.com>
2024-05-28 15:47:05 +0530

    Update tools.tf

--- Terraform/eks-argocd-setup/tools.tf
+++ Terraform/eks-argocd-setup/tools.tf
@@ -8,3 +8,19 @@ resource "helm_release" "argocd" {
   version          = "3.35.4"
   values           = [file("values/argocd.yaml")]
 }
+module "nginx-controller" {
+  source  = "terraform-iaac/nginx-controller/helm"
+
+  additional_set = [
+    {
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+      value = "nlb"
+      type  = "string"
+    },
+    {
+      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-cross-zone-load-balancing-enabled"
+      value = "true"
+      type  = "string"
+    }
+  ]
+}
