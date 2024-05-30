5e68e3778c9a7f1ea4d1d44754a403bea7f7ec73
Narasimha P <pasupuletinarasimha999@gmail.com>
2024-05-30 18:55:24 +0530

    Create test.py

---
+++ Kubernetes/script/test.py
@@ -0,0 +1,22 @@
+import psycopg2
+
+# Database connection parameters
+connection_params = {
+    "dbname": "mydb",
+    "user": "postgres",
+    "password": "Paster813",
+    "host": "terraform-20240530120519907300000001.cxyimaw88ih8.us-east-1.rds.amazonaws.com",
+    "port": "5432"
+}
+
+# Connect to the database
+try:
+  conn = psycopg2.connect(**connection_params)
+  print("Connected to PostgreSQL database!")
+except (Exception, psycopg2.Error) as error:
+  print("Error connecting to PostgreSQL database:", error)
+finally:
+  # Close the connection if it was successful
+  if conn:
+    conn.close()
+    print("Connection closed.")
