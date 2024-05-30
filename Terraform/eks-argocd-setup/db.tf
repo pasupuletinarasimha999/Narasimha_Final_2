4bfcb107fb00b277ca12d51b5260c9941c289bee
Narasimha P <pasupuletinarasimha999@gmail.com>
2024-05-30 20:32:24 +0530

    Update db.tf

--- Terraform/eks-argocd-setup/db.tf
+++ Terraform/eks-argocd-setup/db.tf
@@ -18,6 +18,7 @@ resource "aws_db_instance" "mydb" {
     storage_type                          = "gp2"
     username                              = "postgres"
 	password                              = "Paster813"
+    vpc_security_group_ids = [aws_security_group.dbsg.id]
     tags = {
     Name = "mydb"  # Set the friendly name or identifier here
   }
@@ -26,3 +27,37 @@ resource "aws_db_subnet_group" "mydb_subnet_group" {
   name = "mydb-subnet-group"
   subnet_ids = [aws_subnet.private-us-east-1a.id, aws_subnet.private-us-east-1b.id]
 }   
+
+resource "aws_security_group" "dbsg" {
+    description = "Security Group of Postgres DB"
+    egress      = [
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = null
+            from_port        = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "-1"
+            security_groups  = []
+            self             = false
+            to_port          = 0
+        },
+    ]
+    ingress     = [
+        {
+            cidr_blocks      = []
+            description      = null
+            from_port        = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "-1"
+            security_groups  = [aws_eks_cluster.demo.vpc_config[0].cluster_security_group_id,]
+            self             = false
+            to_port          = 0
+        },
+    ]
+    name        = "DBSG"
+    vpc_id      = aws_vpc.main.id
+}
